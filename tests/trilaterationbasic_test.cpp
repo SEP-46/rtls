dd00ee47afe708cac62754094bac098c60034bc4
raminouri <46562087+raminouri@users.noreply.github.com>
2022-08-24 15:36:20 +1000

    Add basic trilateration tests

---
+++ tests/trilaterationbasic_test.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+
+#include "trilaterationsolver_basic.h"
+
+TEST( TrilaterationBasic, BasicCase )
+{
+	Vec3 anchors[] = {
+		Vec3( 0.0f, 0.0f, 0.0f ),
+		Vec3( 0.5f, 0.0f, 0.0f ),
+		Vec3( 0.0f, 0.5f, 0.0f )
+	};
+
+	// Distances from above anchors to a tag located at (0.5, 0.5)
+	float distances[] = {
+		sqrtf( 0.5f ),
+		0.5f,
+		0.5f
+	};
+
+	TrilaterationSolver_Basic solver;
+	TrilaterationResult result = solver.FindTagPosition( anchors, distances, 3 );
+	EXPECT_TRUE( result.SolutionFound() );
+	EXPECT_TRUE( Vec3::CloseEnough( result.GetPossibleTagPosition( 0 ), Vec3( 0.5f, 0.5f, 0.0f ) ) );
+}
+
+TEST( TrilaterationBasic, NonIntersecting )
+{
+	Vec3 anchors[] = {
+		Vec3( 0.0f, 0.0f, 0.0f ),
+		Vec3( 0.5f, 0.0f, 0.0f ),
+		Vec3( 0.0f, 0.5f, 0.0f )
+	};
+
+	float distances[] = {
+		0.174f,
+		0.23f,
+		0.188f
+	};
+
+	TrilaterationSolver_Basic solver;
+	TrilaterationResult result = solver.FindTagPosition( anchors, distances, 3 );
+	EXPECT_TRUE( result.SolutionFound() );
+}
+
+TEST( TrilaterationBasic, Colinear )
+{
+	Vec3 anchors[] = {
+		Vec3( 0.0f, 0.0f, 0.0f ),
+		Vec3( 0.5f, 0.0f, 0.0f ),
+		Vec3( 1.0f, 0.0f, 0.0f )
+	};
+
+	// Distances from above anchors to a tag located at (0.5, 0.5)
+	float distances[] = {
+		sqrtf( 0.5f ),
+		0.5f,
+		sqrtf( 0.5f )
+	};
+
+	TrilaterationSolver_Basic solver;
+	TrilaterationResult result = solver.FindTagPosition( anchors, distances, 3 );
+	EXPECT_FALSE( result.SolutionFound() );
+}
