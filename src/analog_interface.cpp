50e17d8e1bced1533982307fbe80f6482d1e24a2
Mechail <sephrone_wow@live.com>
2022-09-07 12:06:07 +1000

    Analog Interface v1
    Outputs the X and Y location as an analog voltage difference.
    
    Currently it is set to millimeters, but can be adjusted to centimeters.
    If mm range is limited to 4m, cm range is limited to 40m (Trade-off).
    
    Code might not be perfect due to requiring Linux to compile (needs testing).

--- src/analog_interface.cpp
+++ src/analog_interface.cpp
@@ -21,9 +21,21 @@ int AnalogInterface::Write(const Vec3& data)
 // Linux specific code
 // ...
 #include <ABE_ADCDACPi.h>
+
+static ABElectronics_CPP_Libraries::ADCDACPi gAnalog;
+
 AnalogInterface::AnalogInterface()
 {
+	if (gAnalog.open_dac() != 1) // open the DAC spi channel
+	{
+		return(1); // if the SPI bus fails to open exit the program
+	}
+	adcdac.set_dac_gain(2); // set the dac gain to 2 which will give a voltage range of 0 to 3.3V
+}
 
+~AnalogInterface::AnalogInterface()
+{
+	gAnalog.close_dac();
 }
 
 int AnalogInterface::Read(Vec3* data)
@@ -33,6 +45,9 @@ int AnalogInterface::Read(Vec3* data)
 
 int AnalogInterface::Write(const Vec3& data)
 {
+	// Sets the voltage to match to location data in millimeters
+	gAnalog.set_dac_raw(uint16_t (1000*data.x), uint8_t 1); // X location to Ch1 (converts meters to mm)
+	gAnalog.set_dac_raw(uint16_t (1000*data.y), uint8_t 2); // Y location to Ch2 (converts meters to mm)
 	return 0;
 }
 #endif
