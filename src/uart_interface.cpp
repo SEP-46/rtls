87cd9069434be397ead9e0d635d934c4fb6ac5cd
raminouri <46562087+raminouri@users.noreply.github.com>
2022-09-08 00:30:28 +1000

    Make UART interface opional
    Won't do anything if it fails to connect

--- src/uart_interface.cpp
+++ src/uart_interface.cpp
@@ -3,7 +3,35 @@
 
 static serial::Serial gSerial;
 
-UartInterface::UartInterface( int port )
+UartInterface::UartInterface( int portIndex )
+{
+	std::vector<serial::PortInfo> ports = serial::list_ports();
+	if ( portIndex < ports.size() )
+		ConnectToPort( ports[portIndex].port.c_str() );
+}
+
+UartInterface::UartInterface( const char* portName )
+{
+	ConnectToPort( portName );
+}
+
+int UartInterface::Read( void* data, int length )
+{
+	if ( !gSerial.isOpen() )
+		return 0;
+
+	return (int)gSerial.read( (uint8_t*)data, length );
+}
+
+int UartInterface::Write( const void* data, int length )
+{
+	if ( !gSerial.isOpen() )
+		return 0;
+
+	return (int)gSerial.write( (const uint8_t*)data, length );
+}
+
+void UartInterface::ConnectToPort( const char* portName )
 {
 	gSerial.setBaudrate( 115200 );
 	gSerial.setBytesize( serial::eightbits );
@@ -14,16 +42,6 @@ UartInterface::UartInterface( int port )
 	auto timeout = serial::Timeout::simpleTimeout( 100 );
 	gSerial.setTimeout( timeout );
 
-	std::vector<serial::PortInfo> ports = serial::list_ports();
-	gSerial.setPort( ports[port].port );
-}
-
-int UartInterface::Read( void* data, int length )
-{
-	return (int)gSerial.read( (uint8_t*)data, length );
-}
-
-int UartInterface::Write( const void* data, int length )
-{
-	return (int)gSerial.write( (const uint8_t*)data, length );
+	gSerial.setPort( portName );
+	gSerial.open();
 }
