d4eb48780f8e6972f660f351f0532a0189e07bb9
raminouri <46562087+raminouri@users.noreply.github.com>
2022-09-08 00:30:28 +1000

    Make UART interface opional
    Won't do anything if it fails to connect

--- src/uart_interface.h
+++ src/uart_interface.h
@@ -5,9 +5,12 @@
 class UartInterface : public CommunicationInterface
 {
 public:
-	UartInterface( int port );
+	UartInterface( int portIndex );
+	UartInterface( const char* portName );
 
 	virtual int Read( void* data, int length ) override;
-
 	virtual int Write( const void* data, int length ) override;
+
+private:
+	void ConnectToPort( const char* portName );
 };
