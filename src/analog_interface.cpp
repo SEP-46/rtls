5a0d37ee5e7faa54f4dbff76fb146f6bf541d672
Mechail <sephrone_wow@live.com>
2022-09-17 17:56:56 +1000

    Analog Interface v1.1
    Added a few new things.
    
    Main change is providing the user capabilities to get and set the min/max range of the RTLS area (x, y, z).
    
    It converts these ranges into a resolution for analog output (0 to 4095 volts).
    
    Effectively, an area that is roughly 4x4x4 meters will have an accuracy resolution of ~1mm (40x40x40 ~1cm, etc).

--- src/analog_interface.cpp
+++ src/analog_interface.cpp
@@ -1,22 +1,53 @@
 #include "analog_interface.h"
+#include <cmath>
+
+#define ResCE 244.2002442 // Resolution Coefficent (1000/4.095)
 
 #ifdef WIN32
 // Microsoft specific code
 // ...
 AnalogInterface::AnalogInterface()
 {
-
+	return;
 }
 
 int AnalogInterface::Read(Vec3* data)
 {
 	return 0;
 }
-
 int AnalogInterface::Write(const Vec3& data)
 {
 	return 0;
 }
+
+Vec3 AnalogInterface::GetMinRange()
+{
+	return mMinRange;
+}
+Vec3 AnalogInterface::GetMaxRange()
+{
+	return mMaxRange;
+}
+void AnalogInterface::SetRange(Vec3 aMin, Vec3 aMax)
+{
+	mMinRange = aMin;
+	mMaxRange = aMax;
+	SetResolution();
+}
+void AnalogInterface::SetResolution()
+{
+	Vec3 MagnitudeRange = mMaxRange - mMinRange;
+	mResolution.x = abs(MagnitudeRange.x)*ResCE;
+	mResolution.y = abs(MagnitudeRange.y)*ResCE;
+	mResolution.z = abs(MagnitudeRange.z)*ResCE;
+}
+
+uint16_t AnalogInterface::CastAsInt(float aResAxis, float aTagCoord)
+{
+	float result = aResAxis * aTagCoord;
+	return (uint16_t)result;
+}
+
 #else
 // Linux specific code
 // ...
@@ -26,11 +57,11 @@ static ABElectronics_CPP_Libraries::ADCDACPi gAnalog;
 
 AnalogInterface::AnalogInterface()
 {
-	if (gAnalog.open_dac() != 1) // open the DAC spi channel
+	if (!gAnalog.open_dac()) // open the DAC spi channel
 	{
-		return(1); // if the SPI bus fails to open exit the program
+		return; // if the SPI bus fails to open exit the function
 	}
-	adcdac.set_dac_gain(2); // set the dac gain to 2 which will give a voltage range of 0 to 3.3V
+	adcdac.set_dac_gain(1); // set the dac gain to 1 which will give a voltage range of 0 to 2.048V
 }
 
 ~AnalogInterface::AnalogInterface()
@@ -45,11 +76,40 @@ int AnalogInterface::Read(Vec3* data)
 
 int AnalogInterface::Write(const Vec3& data)
 {
-	// Sets the voltage to match to location data in millimeters
-	gAnalog.set_dac_raw(uint16_t (1000*data.x), uint8_t 1); // X location to Ch1 (converts meters to mm)
-	gAnalog.set_dac_raw(uint16_t (1000*data.y), uint8_t 2); // Y location to Ch2 (converts meters to mm)
+	// Sets the voltage to match to location data
+	gAnalog.set_dac_raw(CastAsInt(mResolution.x, data.x), 1); // X location to Ch1
+	gAnalog.set_dac_raw(CastAsInt(mResolution.y, data.y), 2); // Y location to Ch2
 	return 0;
 }
+
+Vec3 AnalogInterface::GetMinRange()
+{
+	return mMinRange;
+}
+Vec3 AnalogInterface::GetMaxRange()
+{
+	return mMaxRange;
+}
+void AnalogInterface::SetRange(Vec3 aMin, Vec3 aMax)
+{
+	mMinRange = aMin;
+	mMaxRange = aMax;
+	SetResolution();
+}
+void AnalogInterface::SetResolution()
+{
+	Vec3 MagnitudeRange = mMaxRange - mMinRange;
+	mResolution.x = abs(MagnitudeRange.x) * ResCE;
+	mResolution.y = abs(MagnitudeRange.y) * ResCE;
+	mResolution.z = abs(MagnitudeRange.z) * ResCE;
+}
+
+uint16_t AnalogInterface::CastAsInt(float aResAxis, float aTagCoord)
+{
+	float result = aResAxis * aTagCoord;
+	return (uint16_t)result;
+}
+
 #endif
 
 
--- src/analog_interface.h
+++ src/analog_interface.h
@@ -2,6 +2,14 @@
 
 #include "communication_interface.h"
 #include "vector.h"
+#include <cstdint>
+
+struct Resolution
+{
+	float x;
+	float y;
+	float z;
+};
 
 class AnalogInterface : public CommunicationInterface
 {
@@ -9,6 +17,17 @@ public:
 	AnalogInterface();
 
 	virtual int Read( Vec3* data);
-
 	virtual int Write( const Vec3& data);
+
+	Vec3 GetMinRange();
+	Vec3 GetMaxRange();
+	void SetRange(Vec3 aMin, Vec3 aMax);
+	void SetResolution();
+
+	uint16_t CastAsInt(float aResAxis, float aTagCoord);
+
+private:
+	Vec3 mMinRange = { -2.048, -2.048, -2.048 };
+	Vec3 mMaxRange = { 2.048, 2.048, 2.048 };
+	Resolution mResolution = { 1000.0, 1000.0, 1000.0 };
 };
