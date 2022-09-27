fb3035a6f74eca8cc6cddfa8c8fa59844f1a1811
Mechail <sephrone_wow@live.com>
2022-09-27 14:09:37 +1000

    Analog Unit Testing
    All Windows level functions working as expected.

---
+++ tests/analog_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "analog_interface.h"
+
+AnalogInterface testAnalog;
+Vec3 testRangeMin = { 0.0f, 0.0f, 0.0f };
+Vec3 testRangeMax = { 4.2f, 4.0f, 4.1f };
+
+TEST(AnalogTest, CanGetMinAndMaxRange)
+{
+	testAnalog.SetRange(testRangeMin, testRangeMax);
+
+	EXPECT_FLOAT_EQ(testAnalog.GetMinRange().x, 0.0f);
+	EXPECT_FLOAT_EQ(testAnalog.GetMinRange().y, 0.0f);
+	EXPECT_FLOAT_EQ(testAnalog.GetMinRange().z, 0.0f);
+
+	EXPECT_FLOAT_EQ(testAnalog.GetMaxRange().x, 4.2f);
+	EXPECT_FLOAT_EQ(testAnalog.GetMaxRange().y, 4.0f);
+	EXPECT_FLOAT_EQ(testAnalog.GetMaxRange().z, 4.1f);
+}
+
+TEST(AnalogTest, CanTranslate)
+{
+	float value = 0.5f;
+
+	testAnalog.SetRange(testRangeMin, testRangeMax);
+
+	EXPECT_FLOAT_EQ(testAnalog.Translate(value, testRangeMin.x, testRangeMax.x), 487.5);
+	EXPECT_FLOAT_EQ(testAnalog.Translate(value, testRangeMin.y, testRangeMax.y), 511.875);
+	EXPECT_FLOAT_EQ(testAnalog.Translate(value, testRangeMin.z, testRangeMax.z), 499.3902439024);
+}
