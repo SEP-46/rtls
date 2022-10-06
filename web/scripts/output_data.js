dc7e8d4db4c00f5c428736a849b21fd35ffc5a94
raminouri <46562087+raminouri@users.noreply.github.com>
2022-10-06 18:26:30 +1100

    Update output_data.js

--- web/scripts/output_data.js
+++ web/scripts/output_data.js
@@ -1,20 +1,22 @@
-async function fetchAsync (url)
+'use strict';
+
+async function fetchAsync(endpoint)
 {
-    let response = await fetch(url, {mode: "no-cors"});
-    console.log(response);
-    let data = await response.text();
+    let response = await fetch(endpoint, {mode: "no-cors"});
+    let data = await response.json();
     return data;
 }
 
 async function updateAnchors()
 {
     var A0 = document.getElementById("A0PosOutput");
-    // var A1 = document.getElementById("A1PosOutput");
-    // var A2 = document.getElementById("A2PosOutput");
+    var A1 = document.getElementById("A1PosOutput");
+    var A2 = document.getElementById("A2PosOutput");
 
-    A0.textContent = await fetchAsync('http://localhost/anchors');
-    // A1.textContent = await fetchAsync('http://localhost/anchors');
-    // A2.textContent = await fetchAsync('http://localhost/anchors');
+    const anchors = await fetchAsync('/anchors');
+    A0.textContent = "{" + anchors[0].pos.x + ", " + anchors[0].pos.y + ", " + anchors[0].pos.z + "}";
+    A1.textContent = "{" + anchors[1].pos.x + ", " + anchors[1].pos.y + ", " + anchors[1].pos.z + "}";
+    A2.textContent = "{" + anchors[2].pos.x + ", " + anchors[2].pos.y + ", " + anchors[2].pos.z + "}";
 }
 
 function init()
