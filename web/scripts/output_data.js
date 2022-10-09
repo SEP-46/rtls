081a962d7357a35310471385b47e6dc6cedf9a83
Mechail <sephrone_wow@live.com>
2022-10-09 16:57:11 +1100

    Anchor Positions, Tag Location and Speed working on 'testclient.html'

--- web/scripts/output_data.js
+++ web/scripts/output_data.js
@@ -1,5 +1,54 @@
 'use strict';
 
+let socket = new WebSocket("ws://localhost:9002");
+socket.onopen = function()
+{
+    console.log('socket conected');
+}
+socket.onerror = function(error)
+{
+    alert(`[error] ${error.message}`);
+}
+socket.onclose = function()
+{
+    console.log('socket closed');
+}
+socket.onmessage = function(event)
+{
+    let messageArr = event.data.split(',');
+
+    const tagPosX = parseFloat(messageArr[0]).toFixed(3);
+    const tagPosY = parseFloat(messageArr[1]).toFixed(3);
+    const tagPosZ = parseFloat(messageArr[2]).toFixed(3);
+
+    const timeNew = Date.now();
+    const tagSpeedX = 1000*(tagPosX - CalcSpeed.xOld)/(timeNew - CalcSpeed.timeOld);
+    const tagSpeedY = 1000*(tagPosY - CalcSpeed.yOld)/(timeNew - CalcSpeed.timeOld);
+    const tagSpeedZ = 1000*(tagPosZ - CalcSpeed.zOld)/(timeNew - CalcSpeed.timeOld);
+    CalcSpeed.xOld = tagPosX;
+    CalcSpeed.yOld = tagPosY;
+    CalcSpeed.zOld = tagPosZ;
+    CalcSpeed.timeOld = timeNew;
+    const tagSpeedMag = Math.sqrt((tagSpeedX*tagSpeedX)+(tagSpeedY*tagSpeedY)+(tagSpeedZ*tagSpeedZ)).toFixed(3);
+
+    var TagPosOutput = document.getElementById("TagPosOutput");
+    TagPosOutput.textContent = "{" + tagPosX + ", " + tagPosY + ", " + tagPosZ + "}";
+
+    var TagSpeedOutput = document.getElementById("TagSpeedOutput");
+    TagSpeedOutput.textContent = tagSpeedMag + " m/s";
+}
+
+class CalcSpeed
+{
+    constructor()
+    {
+        xOld = 0.000;
+        yOld = 0.000;
+        zOld = 0.000;
+        timeOld = Date.now();
+    }
+}
+
 async function fetchAsync(endpoint)
 {
     let response = await fetch(endpoint, {mode: "no-cors"});
@@ -21,8 +70,8 @@ async function updateAnchors()
 
 function init()
 {
-    var clickme = document.getElementById("clickme");
-	clickme.onclick = updateAnchors;	
+    var bUpdate = document.getElementById("bUpdate");
+	bUpdate.onclick = updateAnchors;	
 }
 
 window.onload = init;
